4259ec33ad4d6a1ebeff39329b5115914301fea1
samhirsch0109 <sammyhirsch0109@gmail.com>
2024-09-30 08:46:35 -0400

    finished tutorial

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.swatch').on('mouseenter', function () {
+        $('#product-photo').attr('src', 'img/chair-' + $(this).attr('id') + '@2x.jpg');
+        $('.swatch').removeClass('selected');
+        $(this).addClass('selected');
+
+    })
+})
